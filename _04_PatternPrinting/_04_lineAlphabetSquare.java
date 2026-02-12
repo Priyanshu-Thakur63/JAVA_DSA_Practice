50575ac530b7338230ff18322e3f8f54316f016f
Priyanshu Kumar <thatcoldcoffee17100@gmail.com>
2026-02-13 01:20:16 +0530

    adding files in pattern printing

---
+++ _04_PatternPrinting/_04_lineAlphabetSquare.java
@@ -0,0 +1,19 @@
+package _04_PatternPrinting;
+
+import java.util.Scanner;
+
+public class _04_lineAlphabetSquare {
+
+	public static void main(String[] args) {
+		Scanner sc= new Scanner(System.in);
+		System.out.println("Enter a number :");
+		int n= sc.nextInt();
+		for(int i=1; i<=n; i++) {
+			for(int j=1; j<=n; j++) {
+				System.out.print((char)(65+i-1)+" ");
+			}
+			System.out.println();
+		}
+	}
+
+}
